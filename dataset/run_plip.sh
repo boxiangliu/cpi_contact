266aeb059bd84c96f9fd0d191fabb3d2c17efe6a
Boxiang Liu <jollier.liu@gmail.com>
2021-06-22 09:35:16 -0700

    modified:   dataset/run_plip.sh

--- dataset/run_plip.sh
+++ dataset/run_plip.sh
@@ -1,16 +1,15 @@
 in_dir=$1
-# in_dir = 
 out_dir=$2
-# out_dir=./plip_results/
+export -f $out_dir
 mkdir -p $out_dir
-cd $out_dir
+
 
 
 run_plip(){
 	f=$1
 	base=$(basename $f .pdb)
 	echo $base
-	plipcmd.py -f $f -t --name ${base}_output
+	plipcmd.py -f $f -t --name $out_dir/${base}_output
 }
 export -f run_plip
 
