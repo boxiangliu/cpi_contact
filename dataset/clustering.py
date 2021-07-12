44ec8583e09c0f27aa07850a1d6b55a8d9dba19f
Boxiang Liu <jollier.liu@gmail.com>
2021-07-12 16:29:35 -0700

    modified:   dataset/clustering.py 	modified:   utils.py

--- dataset/clustering.py
+++ dataset/clustering.py
@@ -1,14 +1,57 @@
 import sys
+from utils import DataUtils, config_fn, aa_list
+import os
+import pickle
+from rdkit import Chem
+from collections import defaultdict
 
-class Preprocessor():
-    def __init__(self, measure="IC50"):
-        self.get_measure()
+class Preprocessor(DataUtils):
+    def __init__(self, config_fn, measure="IC50"):
+        super(Preprocessor, self).__init__(config_fn)
+        self.set_measure(measure)
+        self.mol_dict = self.get_mol_dict()
+        self.interaction_dict = self.get_interaction_dict()
+        self.word_dict = self.get_word_dict()
 
-
-    def get_measure(self, measure):
+    def set_measure(self, measure):
         assert measure in ["IC50", "KIKD"]
         self.measure = measure
         sys.stderr.write(f"Creating dataset for measurement: {measure}\n")
 
     def get_mol_dict(self):
-        
+        mol_dict_fn = self.config["PDB"]["MOL_DICT"]
+        coordinate_fn = self.config["PDB"]["COORDINATE"]
+
+        if os.path.exists(mol_dict_fn):
+            with open(mol_dict_fn, "rb") as f:
+                mol_dict = pickle.load(f)
+        else:
+            mol_dict = {}
+            mols = Chem.SDMolSupplier(coordinate_fn)
+            for m in mols:
+                if m is None:
+                    continue
+                name = m.GetProp("_Name")
+                mol_dict[name] = m
+        with open(mol_dict_fn, 'wb') as f:
+            pickle.dump(mol_dict, f)
+
+        sys.stderr.write("Finished loading mol_dict.\n")
+        return mol_dict
+
+    def get_interaction_dict(self):
+        wd = self.config["DATA"]["WD"]
+        interaction_dict_fn = os.path.join(wd, "out7_final_pairwise_interaction_dict")
+        with open(interaction_dict_fn, "rb") as f:
+            interaction_dict = pickle.load(f)
+
+        sys.stderr.write("Finished loading interaction dict.\n")
+        return interaction_dict
+
+    def get_word_dict(self):
+        word_dict = defaultdict(lambda: len(word_dict))
+        for aa in aa_list:
+            word_dict[aa]
+        word_dict["X"]
+        return word_dict
+preprocessor = Preprocessor(config_fn)
--- utils.py
+++ utils.py
@@ -5,6 +5,15 @@ import os
 import sys
 
 config_fn = "config.yaml"
+elem_list = ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', \
+    'Na', 'Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', \
+    'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H', 'Li', 'Ge', \
+    'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr', 'Pt', 'Hg', 'Pb', \
+    'W', 'Ru', 'Nb', 'Re', 'Te', 'Rh', 'Tc', 'Ba', 'Bi', 'Hf', 'Mo', \
+    'U', 'Sm', 'Os', 'Ir', 'Ce','Gd','Ga','Cs', 'unknown']
+aa_list = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',\
+    'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+
 
 class DataUtils():
 
