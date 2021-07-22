f362c9650f96ad42b88f5e4d8e8243a6dcd97631
Boxiang Liu <jollier.liu@gmail.com>
2021-07-22 16:36:10 -0700

    debug

--- pipe.sh
+++ pipe.sh
@@ -30,3 +30,6 @@ sw_dir=dataset/smith-waterman-src/
 fasta_fn=../data/results/out6.1_target_uniprot_pdb.fasta
 out_fn=../data/results/out6.1_target_uniprot_pdb_align.txt
 python2 ${sw_dir}/pyssw.py -l $sw_dir -c -p $fasta_fn $fasta_fn > $out_fn
+
+
+python model/train.py IC50 new_compound 0.3
