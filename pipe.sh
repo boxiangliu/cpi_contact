d7ddb2bf3f93f3e0943488b9ed8cf810df3fcca8
Boxiang Liu <jollier.liu@gmail.com>
2021-08-04 13:35:21 -0700

    modified:   config/config.yaml 	modified:   engine/trainer.py 	modified:   pipe.sh 	renamed:    data/MSA/bak/a3m_to_1hot.py -> preprocess/MSA/bak/a3m_to_1hot.py 	renamed:    data/MSA/bak/extract_msa_feature.py -> preprocess/MSA/bak/extract_msa_feature.py 	renamed:    data/MSA/cleanup.py -> preprocess/MSA/cleanup.py 	renamed:    data/MSA/extract_msa_features.py -> preprocess/MSA/extract_msa_features.py 	renamed:    data/MSA/extract_msa_features.sh -> preprocess/MSA/extract_msa_features.sh 	renamed:    data/MSA/hhblits_input.py -> preprocess/MSA/hhblits_input.py 	renamed:    data/MSA/run_hhblits.sh -> preprocess/MSA/run_hhblits.sh 	renamed:    data/MSA/seq_length.py -> preprocess/MSA/seq_length.py 	renamed:    data/get_alignment.py -> preprocess/get_alignment.py 	renamed:    data/get_alignment_score.py -> preprocess/get_alignment_score.py 	renamed:    data/get_alignment_score.sh -> preprocess/get_alignment_score.sh 	renamed:    data/get_dataset.py -> preprocess/get_dataset.py 	renamed:    data/get_final_interaction.py -> preprocess/get_final_interaction.py 	renamed:    data/get_interaction.py -> preprocess/get_interaction.py 	renamed:    data/preprocess_and_clustering.py -> preprocess/preprocess_and_clustering.py 	renamed:    data/run_plip.sh -> preprocess/run_plip.sh 	renamed:    data/smith-waterman-src/Makefile -> preprocess/smith-waterman-src/Makefile 	renamed:    data/smith-waterman-src/example.c -> preprocess/smith-waterman-src/example.c 	renamed:    data/smith-waterman-src/example.cpp -> preprocess/smith-waterman-src/example.cpp 	renamed:    data/smith-waterman-src/kseq.h -> preprocess/smith-waterman-src/kseq.h 	renamed:    data/smith-waterman-src/libssw.so -> preprocess/smith-waterman-src/libssw.so 	renamed:    data/smith-waterman-src/main.c -> preprocess/smith-waterman-src/main.c 	renamed:    data/smith-waterman-src/pyssw.py -> preprocess/smith-waterman-src/pyssw.py 	renamed:    data/smith-waterman-src/pyssw_pairwise.py -> preprocess/smith-waterman-src/pyssw_pairwise.py 	renamed:    data/smith-waterman-src/ssw.c -> preprocess/smith-waterman-src/ssw.c 	renamed:    data/smith-waterman-src/ssw.h -> preprocess/smith-waterman-src/ssw.h 	renamed:    data/smith-waterman-src/ssw.o -> preprocess/smith-waterman-src/ssw.o 	renamed:    data/smith-waterman-src/ssw_cpp.cpp -> preprocess/smith-waterman-src/ssw_cpp.cpp 	renamed:    data/smith-waterman-src/ssw_cpp.h -> preprocess/smith-waterman-src/ssw_cpp.h 	renamed:    data/smith-waterman-src/ssw_lib.py -> preprocess/smith-waterman-src/ssw_lib.py 	renamed:    data/smith-waterman-src/ssw_lib.pyc -> preprocess/smith-waterman-src/ssw_lib.pyc 	renamed:    data/smith-waterman-src/ssw_test -> preprocess/smith-waterman-src/ssw_test 	renamed:    data/smith-waterman-src/sswjni.c -> preprocess/smith-waterman-src/sswjni.c

--- pipe.sh
+++ pipe.sh
@@ -3,36 +3,36 @@
 ##################
 
 # Download data:
-python data/get_dataset.py
+python preprocess/get_dataset.py
 
 # Use PLIP to get compound-protein interaction:
-bash data/run_plip.sh ../data/pdb_files/ ../data/plip_results/
+bash preprocess/run_plip.sh ../data/pdb_files/ ../data/plip_results/
 
 # Parse PLIP compound-protein interaction result: 
-python dataset/get_interaction.py
+python preprocess/get_interaction.py
 
 # Align PDBBind and UniProt protein sequences:
-python dataset/get_alignment.py
+python preprocess/get_alignment.py
 
 # Use alignment to adjust interacting residues: 
-python dataset/get_final_interaction.py
+python preprocess/get_final_interaction.py
 
 
 # Generate MSA features:
 # Generate fasta files:
-python data/MSA/hhblits_input.py ../data/results/out7_final_pairwise_interaction_dict ../processed_data/MSA/
+python preprocess/MSA/hhblits_input.py ../data/results/out7_final_pairwise_interaction_dict ../processed_data/MSA/
 
 # Run hhblits:
-bash data/MSA/run_hhblits.sh ../processed_data/MSA ../processed_data/MSA
+bash preprocess/MSA/run_hhblits.sh ../processed_data/MSA ../processed_data/MSA
 
 # Extract MSA features using MSA transformer:
-bash data/MSA/extract_msa_features.sh
+bash preprocess/MSA/extract_msa_features.sh
 
 # Get alignment score:
-bash get_alignment_score.sh
+bash preprocess/get_alignment_score.sh
 
 # Final processing and clustering for train-dev-test split:
-python preprocess_and_clustering.py
+python preprocess/preprocess_and_clustering.py
 
 
 
