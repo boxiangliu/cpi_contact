6c24cf900c571ab96a399d60090c6277d6db70f7
Boxiang Liu <jollier.liu@gmail.com>
2021-08-09 19:32:06 -0700

    debug

--- engine/trainer.py
+++ engine/trainer.py
@@ -15,7 +15,7 @@ from torch.utils.data import Dataset, DataLoader
 import time
 from tensorboardX import SummaryWriter
 from torch import optim
-from data.dataset import CPIDataset
+from data.dataset import CPIDataset, collate_cpi
 
 elem_list = ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr', 'Pt', 'Hg', 'Pb', 'W', 'Ru', 'Nb', 'Re', 'Te', 'Rh', 'Tc', 'Ba', 'Bi', 'Hf', 'Mo', 'U', 'Sm', 'Os', 'Ir', 'Ce','Gd','Ga','Cs', 'unknown']
 atom_fdim = len(elem_list) + 6 + 6 + 6 + 1
