47ab5738965284218f794e20c11d18182e61042f
Boxiang Liu <jollier.liu@gmail.com>
2021-08-10 22:52:10 -0700

    modified:   engine/trainer.py

--- engine/trainer.py
+++ engine/trainer.py
@@ -18,7 +18,7 @@ from torch import optim
 from data.dataset import CPIDataset, collate_cpi
 import logging
 from sklearn.metrics import mean_squared_error, precision_score, roc_auc_score
-import stats
+from scipy import stats
 
 
 elem_list = ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr', 'Pt', 'Hg', 'Pb', 'W', 'Ru', 'Nb', 'Re', 'Te', 'Rh', 'Tc', 'Ba', 'Bi', 'Hf', 'Mo', 'U', 'Sm', 'Os', 'Ir', 'Ce','Gd','Ga','Cs', 'unknown']
