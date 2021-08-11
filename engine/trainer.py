30fbecba53ad885cd6270bfec012f994cafdda03
Boxiang Liu <jollier.liu@gmail.com>
2021-08-11 15:36:03 -0700

    debug

--- engine/trainer.py
+++ engine/trainer.py
@@ -24,7 +24,6 @@ from shutil import copyfile
 elem_list = ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr', 'Pt', 'Hg', 'Pb', 'W', 'Ru', 'Nb', 'Re', 'Te', 'Rh', 'Tc', 'Ba', 'Bi', 'Hf', 'Mo', 'U', 'Sm', 'Os', 'Ir', 'Ce','Gd','Ga','Cs', 'unknown']
 atom_fdim = len(elem_list) + 6 + 6 + 6 + 1
 bond_fdim = 6
-CFG = "config/config.yaml"
 
 class Trainer(object):
     def __init__(self, args, train_data, valid_data, test_data):
