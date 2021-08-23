7c8beab85d5f6283d603933cdece0f785b1b9a7d
Boxiang Liu <jollier.liu@gmail.com>
2021-08-23 14:36:59 -0700

    debug

--- preprocess/AF2/unique_fasta.py
+++ preprocess/AF2/unique_fasta.py
@@ -10,7 +10,7 @@ if os.path.exists(uniq_dir):
 
 uniprot_list = []
 for file in glob.glob(msa_dir + "/*.fasta"):
-    uniprot_id = os.path.basename(file).replace(".fasta").split("_")[1]
+    uniprot_id = os.path.basename(file).replace(".fasta", "").split("_")[1]
     if uniprot_id not in uniprot_list:
         uniprot_list.append(uniprot_id)
         copyfile(file, os.path.join(uniq_dir, "{}.fasta".format(uniprot_id)))
