8f8a31f4f3986575b3932ef40e5dc05ed3c76567
Boxiang Liu <jollier.liu@gmail.com>
2021-08-24 14:11:33 -0700

    debug

--- preprocess/AF2/submit_slurm.sh
+++ preprocess/AF2/submit_slurm.sh
@@ -1,9 +1,10 @@
 partition="1080Ti,1080Ti_mlong,1080Ti_short,1080Ti_slong,2080Ti,2080Ti_mlong,CPUx16,CPUx40,M40x8,M40x8_mlong,M40x8_slong,P100,TitanXx8,TitanXx8_mlong,TitanXx8_slong,V100_DGX,V100x8"
 
-for file in /mnt/scratch/boxiang/projects/cpi_contact/processed_data/unique_fasta/*fasta; do
-    base=$(basename $file \.fasta)
+# for file in /mnt/scratch/boxiang/projects/cpi_contact/processed_data/unique_fasta/*fasta; do
+for file in /mnt/scratch/boxiang/projects/alphafold/T1084.fasta; do
+    base=$(basename $file .fasta)
     echo $base
     if [[ ! -f /mnt/scratch/boxiang/projects/cpi_contact/processed_data/AF2/$base ]]; then 
-        sbatch --ntasks=1 --cpus-per-task=8 --partition=$partition --job-name=$base --wrap "bash /mnt/scratch/boxiang/projects/cpi_contact/scripts/preprocess/AF2/run_alphafold2.sh $file /mnt/scratch/boxiang/projects/cpi_contact/processed_data/AF2/" --output=/mnt/scratch/boxiang/projects/cpi_contact/processed_data/AF2/${base}.out
+        sbatch --ntasks=1 --cpus-per-task=8 --partition=$partition --job-name=$base --wrap "bash /mnt/scratch/boxiang/projects/alphafold/run_alphafold.sh $file /mnt/scratch/boxiang/projects/cpi_contact/processed_data/AF2/" --output=/mnt/scratch/boxiang/projects/cpi_contact/processed_data/AF2/${base}.out
     fi
 done
