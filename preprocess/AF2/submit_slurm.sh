3d45e1001db0791ec6d8331d644457c13416c150
Boxiang Liu <jollier.liu@gmail.com>
2021-08-31 15:43:42 -0700

    modified:   preprocess/AF2/submit_slurm.sh

--- preprocess/AF2/submit_slurm.sh
+++ preprocess/AF2/submit_slurm.sh
@@ -1,4 +1,5 @@
-partition="1080Ti,1080Ti_mlong,1080Ti_short,1080Ti_slong,2080Ti,2080Ti_mlong,CPUx16,CPUx40,M40x8,M40x8_mlong,M40x8_slong,P100,TitanXx8,TitanXx8_mlong,TitanXx8_slong,V100_DGX,V100x8"
+# partition="1080Ti,1080Ti_mlong,1080Ti_short,1080Ti_slong,2080Ti,2080Ti_mlong,CPUx16,CPUx40,M40x8,M40x8_mlong,M40x8_slong,P100,TitanXx8,TitanXx8_mlong,TitanXx8_slong,V100_DGX,V100x8"
+partition="1080Ti_mlong,1080Ti_slong,2080Ti_mlong,CPUx16,M40x8_mlong,M40x8_slong,TitanXx8_mlong,TitanXx8_slong"
 
 n=0
 for file in /mnt/storage/idl-0/bio/boxiang/cpi_contact/processed_data/unique_fasta/*fasta; do
@@ -16,3 +17,13 @@ for file in /mnt/storage/idl-0/bio/boxiang/cpi_contact/processed_data/unique_fas
         sbatch --ntasks=1 --cpus-per-task=8 --partition=$partition --job-name=$base --wrap "bash /mnt/scratch/boxiang/projects/alphafold/run_alphafold.sh $file /mnt/storage/idl-0/bio/boxiang/cpi_contact/processed_data/AF2/" --output=/mnt/storage/idl-0/bio/boxiang/cpi_contact/processed_data/AF2/${base}.out
     fi
 done
+
+
+
+# unfinished_file=$(grep -L "Final timing" *out | cut -f1 -d":" | sed "s/.out//" | tr "\n" " ")
+
+# for file in $unfinished_file; do 
+#     echo $file
+#     rm -r $file
+#     rm ${file}.out
+# done
